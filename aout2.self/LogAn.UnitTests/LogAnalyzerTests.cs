49ddda76f0b8e01e987025c68d95487a8328edb4
Sergey Rubtsov <rsv.astana@gmail.com>
2017-01-05 22:26:57 +0600

    Первый проходящий тест

---
+++ aout2.self/LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidLogFileName_BadExtension_ReturnsFalse()
+        {
+            var logAnanlyzer = new LogAnanlyzer();
+
+            var result = logAnanlyzer.IsValidLogFileName("dasladlfkj.foo");
+
+            Assert.False(result);
+        }
+    }
+}
--- aout2.self/LogAn/LogAnanlyzer.cs
+++ aout2.self/LogAn/LogAnanlyzer.cs
@@ -4,7 +4,7 @@
     {
         public bool IsValidLogFileName(string fileName)
         {
-            if (fileName.EndsWith(".SLF"))
+            if (!fileName.EndsWith(".SLF"))
             {
                 return false;
             }
