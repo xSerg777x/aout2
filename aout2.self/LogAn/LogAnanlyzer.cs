d6166296e84af984dd36261bf1a1bbb36c09d46d
Sergey Rubtsov <rsv.astana@gmail.com>
2017-01-05 22:02:44 +0600

    Собственное решение

---
+++ aout2.self/LogAn/LogAnanlyzer.cs
@@ -0,0 +1,14 @@
+﻿namespace LogAn
+{
+    public class LogAnanlyzer
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            if (fileName.EndsWith(".SLF"))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
